8fde7bb28da50c5465c1be1141f01752649b2fd1
Brian Crafton <crafton.b@husky.neu.edu>
2016-02-24 20:12:14 -0500

    for discovery cluter, print out accuracy and stuff

--- main.c
+++ main.c
@@ -14,22 +14,23 @@ int main(int argc,char **argv) {
 	unsigned int num_layers = 3;
 	unsigned int num_labels = 10;
 	double lambda = 0.8;
-	/*
+	
+	
 	if(argc < 2)
 	{
 		printf("need exactly 1 argument for vector length\n");
 		return 0;
 	}
-    unsigned int iteration_number = atoi(argv[1]);
-    */
+    	unsigned int iteration_number = atoi(argv[1]);
+   	
 
-	unsigned int iteration_number = 100;
+	//unsigned int iteration_number = 10000;
 
 	matrix_list_t* theta = matrix_list_constructor(2);
-	theta->matrix_list[0] = matrix_random(25, 401, .12);
-	theta->matrix_list[1] = matrix_random(10, 26, .12);
-	//theta->matrix_list[0] = load_from_file("theta1.csv", 25, 401);
-	//theta->matrix_list[1] = load_from_file("theta2.csv", 10, 26);
+	//theta->matrix_list[0] = matrix_random(25, 401, .12);
+	//theta->matrix_list[1] = matrix_random(10, 26, .12);
+	theta->matrix_list[0] = load_from_file("theta1.csv", 25, 401);
+	theta->matrix_list[1] = load_from_file("theta2.csv", 10, 26);
 	
 	assert(theta->num == 2);
 	assert(theta->matrix_list[0]->rows == 25 && theta->matrix_list[0]->cols == 401);
@@ -41,15 +42,7 @@ int main(int argc,char **argv) {
 	matrix_t* y = matrix_transpose(tmp);
 	free_matrix(tmp);
 
-	clock_t start, end;
-	double cpu_time_used;
-	start = clock();
-
 	gradient_descent(rolled_theta, layer_sizes, num_layers, num_labels, X, y, lambda, iteration_number);
-
-	end = clock();
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("time used: %f", cpu_time_used);
 
 	free_matrix(X);
 	free_matrix(y);
