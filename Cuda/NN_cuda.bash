6ea5f17417fabb44cef6a172fc6e079168a13474
bcrafton <crafton.b@husky.neu.edu>
2016-03-30 20:36:19 -0400

    adding the bash script

---
+++ Cuda/NN_cuda.bash
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#BSUB -J brian_cuda
+#BSUB -o output
+#BSUB -e error
+#BSUB -n 1
+#BSUB -q par-gpu
+#BSUB cwd /home/crafton.b/cuda_nn
+cd /home/crafton.b/cuda_nn
+
+rm output
+rm error
+
+nvcc --gpu-architecture=sm_20 --relocatable-device-code=true main.cu matrix.cu vector.cu matrix_list.cu device_matrix.cu device_matrix_list.cu device_vector.cu buffer.cu matrix_util.cu device_matrix_util.cu NeuralNetwork.cu
+./a.out
